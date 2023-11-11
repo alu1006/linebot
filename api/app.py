9315c6faa42d995583c8589d2fff317b197f52ed
alu1006 <andy8050802@gmail.com>
2023-11-11 23:20:18 +0800

    vercel.json

--- api/app.py
+++ api/app.py
@@ -25,55 +25,55 @@ def home():
     return 'Hello, World!'
 
 
-@app.route("/webhook", methods=['POST'])
-def callback():
-    # X-Line-Signature 是用來確認請求是由 Line 伺服器發送的
-    signature = request.headers['X-Line-Signature']
+# @app.route("/webhook", methods=['POST'])
+# def callback():
+#     # X-Line-Signature 是用來確認請求是由 Line 伺服器發送的
+#     signature = request.headers['X-Line-Signature']
 
-    # 取得請求的內容
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+#     # 取得請求的內容
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
 
-    try:
-        # 驗證簽名，若驗證失敗則拋出 InvalidSignatureError
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+#     try:
+#         # 驗證簽名，若驗證失敗則拋出 InvalidSignatureError
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
 
-    return 'OK'
+#     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 取得使用者傳來的文字訊息
-    user_message = event.message.text
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     # 取得使用者傳來的文字訊息
+#     user_message = event.message.text
     
-    # 回覆相同的文字 + 請輸入網址
-    reply_message = user_message + "，請輸入網址"
+#     # 回覆相同的文字 + 請輸入網址
+#     reply_message = user_message + "，請輸入網址"
 
-    # 若使用者輸入的內容是網址，則使用 Bit.ly API 縮短網址
-    if user_message.startswith("http"):
-        shortened_url = shorten_url(user_message)
-        reply_message = f"輸入的網址已縮短為：{shortened_url}"
+#     # 若使用者輸入的內容是網址，則使用 Bit.ly API 縮短網址
+#     if user_message.startswith("http"):
+#         shortened_url = shorten_url(user_message)
+#         reply_message = f"輸入的網址已縮短為：{shortened_url}"
 
-    # 回覆訊息給使用者
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message)
-    )
+#     # 回覆訊息給使用者
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=reply_message)
+#     )
 
-def shorten_url(long_url):
-    # 使用 Bit.ly API 縮短網址
-    url = "https://api-ssl.bitly.com/v4/shorten"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {BITLY_ACCESS_TOKEN}"
-    }
-    data = {
-        "long_url": long_url
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    shortened_url = response.json()["id"]
-    return shortened_url
+# def shorten_url(long_url):
+#     # 使用 Bit.ly API 縮短網址
+#     url = "https://api-ssl.bitly.com/v4/shorten"
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {BITLY_ACCESS_TOKEN}"
+#     }
+#     data = {
+#         "long_url": long_url
+#     }
+#     response = requests.post(url, headers=headers, data=json.dumps(data))
+#     shortened_url = response.json()["id"]
+#     return shortened_url
 
 if __name__ == "__main__":
     app.run(port='8080')
