349c152da0a4ee9affeb8a97ffa4adbc2e32dff8
alu1006 <andy8050802@gmail.com>
2023-11-12 19:56:49 +0800

    vercel.json

--- api/app.py
+++ api/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 # 設定 Line Bot 的 Channel Access Token 和 Channel Secret
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 # # 設定 Bit.ly 的 Access Token
 BITLY_ACCESS_TOKEN = os.getenv('BITLY_ACCESS_TOKEN')
