71bfea8983b151ebeb9b0e9b78698bc58ae9f8b7
alu1006 <andy8050802@gmail.com>
2023-11-12 20:40:12 +0800

    default

--- api/app.py
+++ api/app.py
@@ -1,77 +1,58 @@
-from flask import Flask
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-# from dotenv import load_dotenv
-import os
-
-
 from flask import Flask, request, abort
 
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 
 app = Flask(__name__)
 
-
-line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('CHANNEL_SECRET')
-# # 設定 Bit.ly 的 Access Token
-BITLY_ACCESS_TOKEN = os.getenv('BITLY_ACCESS_TOKEN')
-
-# domain root
-@app.route('/')
-def home():
-    return 'Hello, World!'
+configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-# @app.route("/webhook", methods=['POST'])
-# def callback():
-#     # X-Line-Signature 是用來確認請求是由 Line 伺服器發送的
-#     signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-#     # 取得請求的內容
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     try:
-#         # 驗證簽名，若驗證失敗則拋出 InvalidSignatureError
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-#     return 'OK'
+    return 'OK'
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     # 取得使用者傳來的文字訊息
-#     user_message = event.message.text
-    
-#     # 回覆相同的文字 + 請輸入網址
-#     reply_message = user_message + "，請輸入網址"
 
-#     # 若使用者輸入的內容是網址，則使用 Bit.ly API 縮短網址
-#     if user_message.startswith("http"):
-#         shortened_url = shorten_url(user_message)
-#         reply_message = f"輸入的網址已縮短為：{shortened_url}"
-
-#     # 回覆訊息給使用者
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=reply_message)
-#     )
-
-# def shorten_url(long_url):
-#     # 使用 Bit.ly API 縮短網址
-#     url = "https://api-ssl.bitly.com/v4/shorten"
-#     headers = {
-#         "Content-Type": "application/json",
-#         "Authorization": f"Bearer {BITLY_ACCESS_TOKEN}"
-#     }
-#     data = {
-#         "long_url": long_url
-#     }
-#     response = requests.post(url, headers=headers, data=json.dumps(data))
-#     shortened_url = response.json()["id"]
-#     return shortened_url
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 if __name__ == "__main__":
     app.run()
